bc7c5d31291ec86a8890834e16b5015a08191784
Mikhail Pelagein <retvain@gmail.com>
2021-12-12 19:24:52 +0500

    task 3.10.last stepik golang course

--- main.go
+++ main.go
@@ -3,13 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	//На вход подается целое число. Вам необходимо вывести произведение всех чисел от 1 до данного числа (включительно).
-	//Например, на вход подается число 5, вам нужно найти - 1*2*3*4*5 = 120
-	var num, sum int
-	fmt.Scanln(&num)
-	sum = 1
-	for i := 1; i < num; i++ {
-		sum = sum * (i + 1)
+	//Ваш робот должен принимать на вход 3 целых числа (каждый с новой строки) в диапазоне от 0 до 10 (включительно) и выдавать соответствующий текст на русском языке.
+	var num1 int
+	for i := 1; i <= 3; i++ {
+		fmt.Scanln(&num1)
+		switch num1 {
+		case 0:
+			fmt.Println("Ноль")
+		case 1:
+			fmt.Println("Один")
+		case 2:
+			fmt.Println("Два")
+		case 3:
+			fmt.Println("Три")
+		case 4:
+			fmt.Println("Четыре")
+		case 5:
+			fmt.Println("Пять")
+		case 6:
+			fmt.Println("Шесть")
+		case 7:
+			fmt.Println("Семь")
+		case 8:
+			fmt.Println("Восемь")
+		case 9:
+			fmt.Println("Девять")
+		case 10:
+			fmt.Println("Десять")
+		}
 	}
-	fmt.Println(sum)
 }
