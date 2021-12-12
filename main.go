93b2d07c5d950c969236ee60d9f3b2f229854ffe
Mikhail Pelagein <retvain@gmail.com>
2021-12-12 19:07:16 +0500

    task 3.10 stepik golang course

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var sum int
-	fmt.Scanln(&sum)
-	if sum > 1000 {
-		fmt.Println("Apple")
-	} else if sum >= 500 && sum <= 1000 {
-		fmt.Println("Samsung")
-	} else if sum < 500 {
-		fmt.Println("Nokia с фонариком")
+	//На вход подается целое число. Вам необходимо вывести произведение всех чисел от 1 до данного числа (включительно).
+	//Например, на вход подается число 5, вам нужно найти - 1*2*3*4*5 = 120
+	var num, sum int
+	fmt.Scanln(&num)
+	sum = 1
+	for i := 1; i < num; i++ {
+		sum = sum * (i + 1)
 	}
-
+	fmt.Println(sum)
 }
