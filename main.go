5678d12aed68e2775aa64cca7ecf34e79f47804d
Mikhail Pelagein <retvain@gmail.com>
2021-12-12 22:24:51 +0500

    task 4.6.last stepik golang course

--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func mars_age(a int) float64 {
+	var earthDays, marsAge int
+	earthDays = a * 365
+	marsAge = earthDays / 687
+
+	return float64(marsAge)
+}
 
 func main() {
-	//Ваш робот должен принимать на вход 3 целых числа (каждый с новой строки) в диапазоне от 0 до 10 (включительно) и выдавать соответствующий текст на русском языке.
-	var num1 int
-	for i := 1; i <= 3; i++ {
-		fmt.Scanln(&num1)
-		switch num1 {
-		case 0:
-			fmt.Println("Ноль")
-		case 1:
-			fmt.Println("Один")
-		case 2:
-			fmt.Println("Два")
-		case 3:
-			fmt.Println("Три")
-		case 4:
-			fmt.Println("Четыре")
-		case 5:
-			fmt.Println("Пять")
-		case 6:
-			fmt.Println("Шесть")
-		case 7:
-			fmt.Println("Семь")
-		case 8:
-			fmt.Println("Восемь")
-		case 9:
-			fmt.Println("Девять")
-		case 10:
-			fmt.Println("Десять")
-		}
-	}
+	fmt.Println(mars_age(1000))
 }
